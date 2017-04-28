1837ab4c3175df93891f167dab6344ea0207cea1
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2017-04-28 11:24:03 +0200

    Update TREEMIX.sh

--- TREEMIX.sh
+++ TREEMIX.sh
@@ -3,10 +3,12 @@
 ##### require the file "within" which denote the population labels for plink stratictifation of SNP frequencies
 
 vcf=freebayes.SNPs.filtered.final.recode.intersect.edit.vcf
-plink --vcf $vcf --freq --within within --allow-extra-chr
+plink --vcf $vcf --missing --cluster --freq --within within --allow-extra-chr
 gzip plink.frq
 
 plink2treemix.py plink.frq.gz treemix.frq.gz
+
+treemix -i treemix.frq.gz -k 1000 -m 3 -root ard -o corkwing -se -bootstrap -seed 666
 
 for i 1..10};do
   treemix -i treemix.gz -o $i -k 1000 -m $i -root Ard;
