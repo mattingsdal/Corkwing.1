e640952b4c0d8fb5fd5d766407b4943ead8376fc
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2017-05-15 14:27:22 +0200

    Update ADMIXTURE.sh

--- ADMIXTURE.sh
+++ ADMIXTURE.sh
@@ -12,6 +12,8 @@ mv "admix_names.${K}.P" "${i}_admix_names.${K}.P"
 done
 done
 
+vcftools --het $vcf
+
 # zip Q files and submit to CLUMPAK server
 
 R
@@ -26,6 +28,11 @@ tbl=read.table("admix.3.Q")
 indTable=read.table("pop_info.txt",col.names = c("Sample", "Sex", "Pop"))
 tbl3=cbind(tbl,indTable[,1],indTable[,3])
 
+het=read.table("corkwing.het.txt",sep="",header=T)
+index=c(grep("ARD",het[,1]),grep("SM",het[,1]),grep("NH",het[,1]),grep("ST",het[,1]),grep("EG",het[,1]),grep("AR6",het[,1]),grep("TV",het[,1]),grep("GF",het[,1]))
+HET=het[index,]
+
+
 #### K = 3
 index=c(grep("ARD",tbl2[,3]),grep("SM",tbl2[,3]),grep("NH",tbl2[,3]),grep("ST",tbl2[,3]),grep("EG",tbl2[,3]),grep("AR6",tbl2[,3]),grep("TV",tbl2[,3]),grep("GF",tbl2[,3]))
 
@@ -37,8 +44,12 @@ ord3 = tbl3[index,]
 
 ################ PLOT
 Cairo(file="ADMIXTURE.combined.full.pdf",type="pdf",width=160,height=80,units="mm")
-par(mfrow=c(2,1),mar = c(3,4,1,0))
-barplot(t(as.matrix(ord2)), space = c(0),xlim=c(0,80),col=c("#ffd700","#4169e1"),xlab="", names.arg=ord2[,3],ylab="Ancestry",border=NA,las=2,cex.names=0.3,main="K=2",cex=0.5,cex.main=0.5)
-legend("topright",legend=c("Ard","West","South"),fill=c("#ffd700","#ff4500","#4169e1"),bty="n",cex=0.7)
-barplot(t(as.matrix(ord3)), space = c(0),xlim=c(0,80),col=c("#ffd700","#ff4500","#4169e1"),xlab="", names.arg=ord3[,4],ylab="Ancestry",border=NA,las=2,cex.names=0.3,main="K=3",cex=0.5,cex.main=0.5,bty="n")
+par(mfrow=c(3,1),mar = c(3,4,1,0))
+barplot(t(as.matrix(ord2)), space = c(0),xlim=c(0,65),col=c("#ffd700","#4169e1"),xlab="", names.arg=ord2[,3],ylab="Ancestry",border=NA,las=2,cex.names=0.3,main="K=2",cex=0.5,cex.main=0.5)
+barplot(t(as.matrix(ord3)), space = c(0),xlim=c(0,65),col=c("#ffd700","#ff4500","#4169e1"),xlab="", names.arg=ord3[,4],ylab="Ancestry",border=NA,las=2,cex.names=0.3,main="K=3",cex=0.5,cex.main=0.5,bty="n")
+cl=c(rep("#ffd700",7),rep("#ff4500",24),rep("#4169e1",35))
+barplot(t(as.matrix(ord4[,5])),space=0,col="black",xlim=c(0,65),ylim=c(-0.6,0.4),ylab="F",las=2,cex.main=0.5,main="Inbreeding coefficient",cex.names=0.3,cex=0.5,border=NA)
 dev.off()
+
+
+
