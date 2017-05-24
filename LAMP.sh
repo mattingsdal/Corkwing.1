b224f69b1d9d60ebeeb720cfd7aab2283c44219b
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2017-05-24 15:41:45 +0200

    Update LAMP.sh

--- LAMP.sh
+++ LAMP.sh
@@ -1,13 +1,17 @@
-cd /usit/abel/u1/mortema/software/lamp-ld
-# test is largest contig 5 Mb
+cd /usit/abel/u1/mortema/software/lamp.x86_64-unknown-linux-gnu.REL_2_5/test
+chr=SYMME00023145
 
-# order ped file according to sort.txt file
+plink --bfile ~/vcf/haplotypes_name_sort --chr $chr --within within.plink --freq --allow-extra-chr
+awk '$3 == "ard" {print $6}' plink.frq.strat>ard.frq
+awk '$3 == "west" {print $6}' plink.frq.strat>west.frq
+awk '$3 == "south" {print $6}' plink.frq.strat>south.frq
 
-plink --file test --indiv-sort file sort.txt --make-bed --out test2
-plink --bfile test2 --recode --out test2
+plink --bfile ~/vcf/haplotypes_name_sort --chr $chr --recode A --out test --allow-extra-chr
+plink --bfile ~/vcf/haplotypes_name_sort --chr $chr --recode --out test --allow-extra-chr
+awk '{print $4}' test.map >posfile.txt
+cut --delimiter=' ' -f7-  test.raw | sed 's/ /\t/g' | sed 1d | sed 's/NA/-1/g' >test.geno
+../bin/lamp config.txt
 
-perl lait.pl lamp 2 test2.map test2.ped out/
-cd out
-/usit/abel/u1/mortema/software/lamp-ld/bin/lamp/bin/lamp config.txt
-rm -rf tmp
-/usit/abel/u1/mortema/software/lamp-ld/bin/lamp/bin/generategraph.sh ~/software/lamp-ld/out/ancestry.txt
+rm -rf tmp/
+../bin/generategraph.sh ancestry.txt
+
